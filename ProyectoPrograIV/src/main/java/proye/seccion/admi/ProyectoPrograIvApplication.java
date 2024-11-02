fa68aa47b1c3e653d9e98cac06a552c722a82095
francocp <136655331+francocp@users.noreply.github.com>
2024-11-02 16:23:37 -0600

    Add files via upload

---
+++ ProyectoPrograIV/src/main/java/proye/seccion/admi/ProyectoPrograIvApplication.java
@@ -0,0 +1,13 @@
+package proye.seccion.admi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPrograIvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPrograIvApplication.class, args);
+	}
+
+}
---
+++ ProyectoPrograIV/src/test/java/proye/seccion/admi/ProyectoPrograIvApplicationTests.java
@@ -0,0 +1,13 @@
+package proye.seccion.admi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPrograIvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
