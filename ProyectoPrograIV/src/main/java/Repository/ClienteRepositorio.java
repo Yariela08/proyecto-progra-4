9521e6b038272b2498b07f4204c2747dc4bc1075
Yariela08 <114266699+Yariela08@users.noreply.github.com>
2024-11-19 19:18:56 -0600

    Update ClienteRepositorio.java

--- ProyectoPrograIV/src/main/java/Repository/ClienteRepositorio.java
+++ ProyectoPrograIV/src/main/java/Repository/ClienteRepositorio.java
@@ -2,7 +2,7 @@ package Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import ProyectopIV.Entidades.Cliente;
+import Entidades.Cliente;
 
 
 public interface ClienteRepositorio extends JpaRepository<Cliente, Integer>{
