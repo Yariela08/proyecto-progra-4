e1eb63af2837a9e669cb75c48d2671ab8d1d32b3
Yariela08 <114266699+Yariela08@users.noreply.github.com>
2024-11-20 09:30:23 -0600

    Create InventarioServicioImpl.java

---
+++ ProyectoPrograIV/src/main/java/DataBase/impl/InventarioServicioImpl.java
@@ -0,0 +1,32 @@
+package DataBase.impl;
+
+import Entidades.Inventario;
+import DataBase.InventarioServicio;
+
+public class InventarioServicioImpl implements InventarioServicio{
+
+	@Override
+	public Inventario consultarTodos(Integer clave) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Inventario guardar(Inventario entidad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Inventario actualizar(Inventario entidad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void eliminar(Integer clave) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
