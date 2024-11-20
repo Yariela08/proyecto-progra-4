74a3af0bc18054dfbf0b8c42f6575758f912ae51
Yariela08 <114266699+Yariela08@users.noreply.github.com>
2024-11-20 12:48:13 -0600

    Update ProductoServicioImpl.java

--- ProyectoPrograIV/src/main/java/DataBase/impl/ProductoServicioImpl.java
+++ ProyectoPrograIV/src/main/java/DataBase/impl/ProductoServicioImpl.java
@@ -2,31 +2,35 @@ package DataBase.impl;
 
 import Entidades.Producto;
 import DataBase.ProductoServicio;
+import Repository.ProductoRepositorio;
 
 public class ProductoServicioImpl implements ProductoServicio {
+	private final ProductoRepositorio productoRepositorio;
+	
+	public ProductoServicioImpl(ProductoRepositorio productoRepositorio) {
+		this.productoRepositorio = productoRepositorio;
+	}
 
 	@Override
 	public Producto consultarTodos(Integer clave) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return this.productoRepositorio.getReferenceById(clave);
 	}
 
 	@Override
-	public Producto guardar(Producto entidad) {
-		// TODO Auto-generated method stub
-		return null;
+	public Producto guardar(Producto producto) {
+		
+		return this.productoRepositorio.save(producto);
 	}
 
 	@Override
-	public Producto actualizar(Producto entidad) {
-		// TODO Auto-generated method stub
-		return null;
+	public Producto actualizar(Producto producto) {
+		return this.productoRepositorio.save(producto);
 	}
 
 	@Override
 	public void eliminar(Integer clave) {
-		// TODO Auto-generated method stub
-		
+		this.productoRepositorio.deleteById(clave);
 	}
 
 }
