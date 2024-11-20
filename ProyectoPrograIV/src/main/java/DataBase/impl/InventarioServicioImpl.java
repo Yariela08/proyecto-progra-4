0ae5ebacc5e640110ca014046ef0c217db517781
Yariela08 <114266699+Yariela08@users.noreply.github.com>
2024-11-20 12:40:53 -0600

    Update InventarioServicioImpl.java

--- ProyectoPrograIV/src/main/java/DataBase/impl/InventarioServicioImpl.java
+++ ProyectoPrograIV/src/main/java/DataBase/impl/InventarioServicioImpl.java
@@ -2,30 +2,38 @@ package DataBase.impl;
 
 import Entidades.Inventario;
 import DataBase.InventarioServicio;
+import Repository.InventarioRepositorio;
 
 public class InventarioServicioImpl implements InventarioServicio{
 
+	
+	private final InventarioRepositorio inventarioRepositorio;
+	
+	
+	public InventarioServicioImpl(InventarioRepositorio inventarioRepositorio) {
+		super();
+		this.inventarioRepositorio = inventarioRepositorio;
+	}
+
 	@Override
 	public Inventario consultarTodos(Integer clave) {
-		// TODO Auto-generated method stub
-		return null;
+	
+		return this.inventarioRepositorio.getReferenceById(clave);
 	}
 
 	@Override
-	public Inventario guardar(Inventario entidad) {
-		// TODO Auto-generated method stub
-		return null;
+	public Inventario guardar(Inventario inventario) {
+		return this.inventarioRepositorio.save(inventario);
 	}
 
 	@Override
-	public Inventario actualizar(Inventario entidad) {
-		// TODO Auto-generated method stub
-		return null;
+	public Inventario actualizar(Inventario inventario) {
+		return this.inventarioRepositorio.save(inventario);
 	}
 
 	@Override
 	public void eliminar(Integer clave) {
-		// TODO Auto-generated method stub
+		this.inventarioRepositorio.deleteById(clave);
 		
 	}
 
