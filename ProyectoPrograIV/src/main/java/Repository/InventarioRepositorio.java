baa1dc25e18f9fab43b94eb76cd352c72912d305
Yariela08 <114266699+Yariela08@users.noreply.github.com>
2024-11-19 19:07:05 -0600

    Create InventarioRepositorio.java

---
+++ ProyectoPrograIV/src/main/java/Repository/InventarioRepositorio.java
@@ -0,0 +1,9 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ProyectopIV.Entidades.Inventario;
+
+
+
+public interface InventarioRepositorio extends JpaRepository<Inventario, Integer>{
+
+}
