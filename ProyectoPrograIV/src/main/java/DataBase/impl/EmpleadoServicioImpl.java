524f21cf5a8cec26cae26da925236c39521e5351
Yariela08 <114266699+Yariela08@users.noreply.github.com>
2024-11-20 09:32:37 -0600

    Create EmpleadoServicioImpl.java

---
+++ ProyectoPrograIV/src/main/java/DataBase/impl/EmpleadoServicioImpl.java
@@ -0,0 +1,32 @@
+package DataBse.impl;
+
+import Entidades.Empleado;
+import DataBase.EmpleadoServicio;
+
+public class EmpleadoServicioImpl implements EmpleadoServicio {
+
+	@Override
+	public Empleado consultarTodos(Integer clave) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Empleado guardar(Empleado entidad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Empleado actualizar(Empleado entidad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void eliminar(Integer clave) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
