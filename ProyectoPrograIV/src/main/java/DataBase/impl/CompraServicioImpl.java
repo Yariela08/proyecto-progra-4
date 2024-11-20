7d259aad59a6b1bc5e9d8be5f2f36684088d136b
Yariela08 <114266699+Yariela08@users.noreply.github.com>
2024-11-20 09:40:13 -0600

    Create CompraServicioImpl.java

---
+++ ProyectoPrograIV/src/main/java/DataBase/impl/CompraServicioImpl.java
@@ -0,0 +1,32 @@
+package DataBase.impl;
+
+import Entidades.Compras;
+import DataBase.CompraServicio;
+
+public class CompraServicioImpl implements CompraServicio {
+
+	@Override
+	public Compras consultarTodos(Integer clave) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Compras guardar(Compras entidad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Compras actualizar(Compras entidad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void eliminar(Integer clave) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
