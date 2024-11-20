c9d1d15d8c9821e03737a3fe240725a106ad3af5
Yariela08 <114266699+Yariela08@users.noreply.github.com>
2024-11-20 09:38:42 -0600

    Create DeCompraServicioImpl.java

---
+++ ProyectoPrograIV/src/main/java/DataBase/impl/DeCompraServicioImpl.java
@@ -0,0 +1,32 @@
+package DataBase.impl;
+
+import Entidades.DetalleCompra;
+import DataBase.DeCompraServicio;
+
+public class DeCompraServicioImpl implements DeCompraServicio {
+
+	@Override
+	public DetalleCompra consultarTodos(Integer clave) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DetalleCompra guardar(DetalleCompra entidad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DetalleCompra actualizar(DetalleCompra entidad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void eliminar(Integer clave) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
