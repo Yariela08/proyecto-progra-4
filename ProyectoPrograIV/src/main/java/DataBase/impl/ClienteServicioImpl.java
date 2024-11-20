a282358f9f35efcaf3884aa80e83b567539997e0
Yariela08 <114266699+Yariela08@users.noreply.github.com>
2024-11-20 12:22:59 -0600

    Update ClienteServicioImpl.java

--- ProyectoPrograIV/src/main/java/DataBase/impl/ClienteServicioImpl.java
+++ ProyectoPrograIV/src/main/java/DataBase/impl/ClienteServicioImpl.java
@@ -2,30 +2,35 @@ package DataBase.impl;
 
 import Entidades.Cliente;
 import DataBase.ClienteServicio;
+import Repository.ClienteRepositorio;
 
 public class ClienteServicioImpl implements ClienteServicio {
+	
+	private final ClienteRepositorio clienteRepositorio;
+	
+	
+	public ClienteServicioImpl(ClienteRepositorio clienteRepositorio) {
+		this.clienteRepositorio = clienteRepositorio;
+	}
 
 	@Override
 	public Cliente consultarTodos(Integer clave) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.clienteRepositorio.getReferenceById(clave);
 	}
 
 	@Override
-	public Cliente guardar(Cliente entidad) {
-		// TODO Auto-generated method stub
-		return null;
+	public Cliente guardar(Cliente cliente) {
+		return this.clienteRepositorio.save(cliente);
 	}
 
 	@Override
-	public Cliente actualizar(Cliente entidad) {
-		// TODO Auto-generated method stub
-		return null;
+	public Cliente actualizar(Cliente cliente) {
+		return this.clienteRepositorio.save(cliente);
 	}
 
 	@Override
 	public void eliminar(Integer clave) {
-		// TODO Auto-generated method stub
+		this.clienteRepositorio.deleteById(clave);
 		
 	}
 
