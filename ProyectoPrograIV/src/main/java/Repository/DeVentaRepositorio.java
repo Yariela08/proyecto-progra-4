efc6b6ca6aa8f04329a82a73d49a059500cb4011
Yariela08 <114266699+Yariela08@users.noreply.github.com>
2024-11-19 19:18:02 -0600

    Update DeVentaRepositorio.java

--- ProyectoPrograIV/src/main/java/Repository/DeVentaRepositorio.java
+++ ProyectoPrograIV/src/main/java/Repository/DeVentaRepositorio.java
@@ -1,7 +1,7 @@
 package Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import ProyectopIV.Entidades.DetalleVenta;
+import Entidades.DetalleVenta;
 
 
 public interface DeVentaRepositorio extends JpaRepository<DetalleVenta, Integer>{
