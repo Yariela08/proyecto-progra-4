2fbb428557cef42d3858b43e8a9b5f2066bb59b8
Yariela08 <114266699+Yariela08@users.noreply.github.com>
2024-11-19 19:08:13 -0600

    Create ProveedorRepositorio.java

---
+++ ProyectoPrograIV/src/main/java/Repository/ProveedorRepositorio.java
@@ -0,0 +1,8 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ProyectopIV.Entidades.Proveedor;
+
+
+public interface ProveedorRepositorio extends JpaRepository<Proveedor, Integer>{
+
+}
