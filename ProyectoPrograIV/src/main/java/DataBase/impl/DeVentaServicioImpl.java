6cd1d86e63dcf2855b9f079abad151bdbf0e08bf
Yariela08 <114266699+Yariela08@users.noreply.github.com>
2024-11-20 09:37:34 -0600

    Create DeVentaServicioImpl.java

---
+++ ProyectoPrograIV/src/main/java/DataBase/impl/DeVentaServicioImpl.java
@@ -0,0 +1,32 @@
+package DataBase.impl;
+
+import Entidades.DetalleVenta;
+import DataBase.DeVentaServicio;
+
+public class DeVentaServicioImpl implements DeVentaServicio {
+
+	@Override
+	public DetalleVenta consultarTodos(Integer clave) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DetalleVenta guardar(DetalleVenta entidad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DetalleVenta actualizar(DetalleVenta entidad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void eliminar(Integer clave) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
