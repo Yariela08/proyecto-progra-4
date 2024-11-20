2bc3c4f0871af261e4051689fafd993be72b511e
Yariela08 <114266699+Yariela08@users.noreply.github.com>
2024-11-20 09:21:51 -0600

    Create ClienteServicioImpl.java

---
+++ ProyectoPrograIV/src/main/java/DataBase/impl/ClienteServicioImpl.java
@@ -0,0 +1,33 @@
+package DataBase.impl;
+
+import Entidades.Cliente;
+import DataBase.ClienteServicio;
+
+public class ClienteServicioImpl implements ClienteServicio {
+
+	@Override
+	public Cliente consultarTodos(Integer clave) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cliente guardar(Cliente entidad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cliente actualizar(Cliente entidad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void eliminar(Integer clave) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
