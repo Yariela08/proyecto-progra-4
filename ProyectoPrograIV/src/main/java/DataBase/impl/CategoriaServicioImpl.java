454853aa33f796b8b1cbd2c6617abd47561a6ddd
Yariela08 <114266699+Yariela08@users.noreply.github.com>
2024-11-19 20:04:07 -0600

    Create CategoriaServicioImpl.java

---
+++ ProyectoPrograIV/src/main/java/DataBase/impl/CategoriaServicioImpl.java
@@ -0,0 +1,33 @@
+package DataBase.impl;
+
+import ProyectopIV.Entidades.Categoria;
+import ProyectopIV.servicio.CategoriaServicio;
+
+public class CategoriaServicioImpl implements CategoriaServicio {
+
+	@Override
+	public Categoria consultarTodos(Integer clave) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Categoria guardar(Categoria entidad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Categoria actualizar(Categoria entidad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void eliminar(Integer clave) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
