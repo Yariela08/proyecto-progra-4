15607e39ce11f327e473f6f1b8f702432f278ada
Yariela08 <114266699+Yariela08@users.noreply.github.com>
2024-11-20 09:36:27 -0600

    Create ProductoServicioImpl.java

---
+++ ProyectoPrograIV/src/main/java/DataBase/impl/ProductoServicioImpl.java
@@ -0,0 +1,32 @@
+package DataBase.impl;
+
+import Entidades.Producto;
+import DataBase.ProductoServicio;
+
+public class ProductoServicioImpl implements ProductoServicio {
+
+	@Override
+	public Producto consultarTodos(Integer clave) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Producto guardar(Producto entidad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Producto actualizar(Producto entidad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void eliminar(Integer clave) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
