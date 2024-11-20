dcf3f9b3ed95b8f60b45ea9706b8c5df675c94e4
Yariela08 <114266699+Yariela08@users.noreply.github.com>
2024-11-20 12:49:12 -0600

    Update ProveedorServicioImpl.java

--- ProyectoPrograIV/src/main/java/DataBase/impl/ProveedorServicioImpl.java
+++ ProyectoPrograIV/src/main/java/DataBase/impl/ProveedorServicioImpl.java
@@ -2,31 +2,38 @@ package DataBase.impl;
 
 import Entidades.Proveedor;
 import DataBase.ProveedorServicio;
+import Repository.ProveedorRepositorio;
 
 public class ProveedorServicioImpl implements ProveedorServicio{
+	private final ProveedorRepositorio proveedorRepositorio;
+	
+	
+	public ProveedorServicioImpl(ProveedorRepositorio proveedorRepositorio) {
+		this.proveedorRepositorio = proveedorRepositorio;
+	}
 
 	@Override
 	public Proveedor consultarTodos(Integer clave) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return this.proveedorRepositorio.getReferenceById(clave);
 	}
 
 	@Override
-	public Proveedor guardar(Proveedor entidad) {
-		// TODO Auto-generated method stub
-		return null;
+	public Proveedor guardar(Proveedor proveedor) {
+		return this.proveedorRepositorio.save(proveedor);
 	}
 
 	@Override
-	public Proveedor actualizar(Proveedor entidad) {
-		// TODO Auto-generated method stub
-		return null;
+	public Proveedor actualizar(Proveedor proveedor) {
+		return this.proveedorRepositorio.save(proveedor);
 	}
 
 	@Override
 	public void eliminar(Integer clave) {
-		// TODO Auto-generated method stub
+	this.proveedorRepositorio.deleteById(clave);
 		
 	}
+
+}
 
 }
