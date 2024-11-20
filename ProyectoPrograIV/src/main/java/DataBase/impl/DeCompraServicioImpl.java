1257185997ec6ca4c7b652a9eb1c115c9a2bd738
Yariela08 <114266699+Yariela08@users.noreply.github.com>
2024-11-20 12:31:14 -0600

    Update DeCompraServicioImpl.java

--- ProyectoPrograIV/src/main/java/DataBase/impl/DeCompraServicioImpl.java
+++ ProyectoPrograIV/src/main/java/DataBase/impl/DeCompraServicioImpl.java
@@ -2,30 +2,37 @@ package DataBase.impl;
 
 import Entidades.DetalleCompra;
 import DataBase.DeCompraServicio;
+import Repository.DeCompraRepositorio;
 
 public class DeCompraServicioImpl implements DeCompraServicio {
+	
+	private final DeCompraRepositorio deCompraRepositorio;
+	
+	public DeCompraServicioImpl(DeCompraRepositorio deCompraRepositorio) {
+		this.deCompraRepositorio = deCompraRepositorio;
+	}
 
 	@Override
 	public DetalleCompra consultarTodos(Integer clave) {
-		// TODO Auto-generated method stub
-		return null;
+	
+		return this.deCompraRepositorio.getReferenceById(clave);
 	}
 
 	@Override
-	public DetalleCompra guardar(DetalleCompra entidad) {
-		// TODO Auto-generated method stub
-		return null;
+	public DetalleCompra guardar(DetalleCompra detalleCompra) {
+		
+		return this.deCompraRepositorio.save(detalleCompra);
 	}
 
 	@Override
-	public DetalleCompra actualizar(DetalleCompra entidad) {
-		// TODO Auto-generated method stub
-		return null;
+	public DetalleCompra actualizar(DetalleCompra detalleCompra) {
+		
+		return this.deCompraRepositorio.save(detalleCompra);
 	}
 
 	@Override
 	public void eliminar(Integer clave) {
-		// TODO Auto-generated method stub
+		this.deCompraRepositorio.deleteById(clave);
 		
 	}
 
