16ac6db920e47049fa0997221932517d8ddaec29
Yariela08 <114266699+Yariela08@users.noreply.github.com>
2024-11-20 15:45:35 -0600

    Create ProductoDto.java

---
+++ ProyectoPrograIV/src/main/java/DataBase/dto/ProductoDto.java
@@ -0,0 +1,47 @@
+package DataBase.dto;
+
+public class ProductoDto {
+
+	private int id;
+	
+	private String nombre;
+
+	
+	
+	public ProductoDto() {
+	
+	}
+
+
+	public ProductoDto(int id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+}
