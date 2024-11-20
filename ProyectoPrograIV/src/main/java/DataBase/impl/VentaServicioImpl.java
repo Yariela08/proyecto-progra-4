4cc31f8e8ccb7952069b364311244a9606aa8c42
Yariela08 <114266699+Yariela08@users.noreply.github.com>
2024-11-20 09:34:12 -0600

    Create VentaServicioImpl.java

---
+++ ProyectoPrograIV/src/main/java/DataBase/impl/VentaServicioImpl.java
@@ -0,0 +1,32 @@
+package DataBase.impl;
+
+import Entidades.Venta;
+import DataBase.VentaServicio;
+
+public class VentaServicioImpl implements VentaServicio{
+
+	@Override
+	public Venta consultarTodos(Integer clave) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Venta guardar(Venta entidad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Venta actualizar(Venta entidad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void eliminar(Integer clave) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
