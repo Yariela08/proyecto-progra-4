1e92a27870daed7d9e8f79db80cb83e7584627ae
Yariela08 <114266699+Yariela08@users.noreply.github.com>
2024-11-19 19:02:41 -0600

    Update and rename PurchaseRepository.java to CompraRepositorio.java

---
+++ ProyectoPrograIV/src/main/java/Repository/CompraRepositorio.java
@@ -0,0 +1,10 @@
+package Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ProyectopIV.Entidades.Compras;
+
+public interface CompraRepositorio extends JpaRepository<Compras, Integer> {
+
+}
+
