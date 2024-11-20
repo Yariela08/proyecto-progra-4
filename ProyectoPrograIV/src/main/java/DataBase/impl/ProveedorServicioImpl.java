66b79a48178bfa044a2cbd09f8b8975af26cf10e
Yariela08 <114266699+Yariela08@users.noreply.github.com>
2024-11-20 09:35:15 -0600

    Create ProveedorServicioImpl.java

---
+++ ProyectoPrograIV/src/main/java/DataBase/impl/ProveedorServicioImpl.java
@@ -0,0 +1,32 @@
+package DataBase.impl;
+
+import Entidades.Proveedor;
+import DataBase.ProveedorServicio;
+
+public class ProveedorServicioImpl implements ProveedorServicio{
+
+	@Override
+	public Proveedor consultarTodos(Integer clave) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Proveedor guardar(Proveedor entidad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Proveedor actualizar(Proveedor entidad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void eliminar(Integer clave) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
